629390f055bd6f97d41b5a1e44adae45c18698ba
Lin68 <hu.lha86@gmail.com>
2019-03-07 14:45:25 +0800

    Fixed CORS issues

--- db-service/src/main/java/com/legend/stock/dbservice/configuration/CorsConfig.java
+++ db-service/src/main/java/com/legend/stock/dbservice/configuration/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig {
         return new WebMvcConfigurerAdapter() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**");
+                registry.addMapping("/**").allowedOrigins("*").allowedMethods("*");
             }
         };
     }
